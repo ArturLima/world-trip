5dbd37ea0a0aad65e4f4ae4ad0af00946e914a5d
Artur <arturpeixotoo@gmail.com>
2022-04-17 22:31:13 -0300

    create index

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 import { HStack, Image } from "@chakra-ui/react";
 
-export function Logo() {
+export function Header() {
   return (
     <HStack mt={["4", "7"]} ml="auto" mr="auto">
       <Image src="images/Logo.svg" />
